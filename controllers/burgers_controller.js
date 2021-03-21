ba29c6af8fd8149236c4fa42d37ac3769b822bde
Zhylow <pk1blue@yahoo.com>
2021-03-21 11:41:02 -0400

    fixed heroku deployment

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,47 +3,54 @@ let router = express.Router();
 const burger = require("../models/burger");
 
 
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      // res.json(data);
-      var hbsObject = { burgers: data };
-      res.render("index", hbsObject);
-    });
-  });
-  
-  router.post("/api/addburger", function(req, res) {
-    burger.create(
-      ["burger_name", "devoured"],
-      [req.body.burger_name, req.body.devoured],
-      function(result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
-      }
-    );
-  });
-  
-  router.put("/api/eatburger/:id/:devoured", function(req, res) {
-    var burgerId = req.params.id;
-    var devoured = req.params.devoured;
-  
-    console.log("burgerId=" + burgerId);
-  
-    burger.update(devoured, burgerId, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
+// router.get("/", function (req, res) {
+//   burger.all(function (data) {
+//     res.json(data);
+//     var hbsObject = { burgers: data };
+//     res.render("index", hbsObject);
+//   });
+// });
 
-  router.delete("/api/burger/:id", (req, res) => {
-    burger.delete({ id: req.params.id }, data => {
-      console.log(data);
-      res.json(data);
-    });
+router.get("/", function (req, res) {
+  burger.all(response => {
+    res.render("index", { burgers: response });
+    console.log(response);
   });
+});
+
+router.post("/api/addburger", function (req, res) {
+  burger.create(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/api/eatburger/:id/:devoured", function (req, res) {
+  var burgerId = req.params.id;
+  var devoured = req.params.devoured;
+
+  console.log("burgerId=" + burgerId);
+
+  burger.update(devoured, burgerId, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burger/:id", (req, res) => {
+  burger.delete({ id: req.params.id }, data => {
+    console.log(data);
+    res.json(data);
+  });
+});
 
 
 module.exports = router;
